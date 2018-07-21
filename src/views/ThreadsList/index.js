eefdc0530451d57706250d39cced188d3a556fd6
keyserfaty <keserfaty@gmail.com>
2018-07-21 18:02:28 -0300

    add card header

--- src/views/ThreadsList/index.js
+++ src/views/ThreadsList/index.js
@@ -8,17 +8,49 @@ import BottomBar from '../../components/BottomBar'
 import Button from '../../components/Button'
 
 const ThreadsList = () => {
+  const empty = false // TODO: Change to see different states
+
   return (
     <View style={styles.container}>
       <Toolbar />
-      <View style={styles.contentContainer}>
-        <Image style={styles.emptyStateImage} source={require('./statics/thread-empty-state.png')} />
-        <Text style={styles.emptyStateText}>
-          Start sharing your memories with
-          friends and family with threads.
-        </Text>
-        <Button primary title='Create new thread' />
-      </View>
+      { empty && (
+        <View style={styles.emptyStateContainer}>
+          <Image style={styles.emptyStateImage} source={require('./statics/thread-empty-state.png')} />
+          <Text style={styles.emptyStateText}>
+            Start sharing your memories with
+            friends and family with threads.
+          </Text>
+          <Button primary title='Create new thread' />
+        </View>
+      )}
+      { !empty && (
+        <View style={styles.contentContainer}>
+          <View style={styles.threadCard}>
+            <View style={styles.threadCardHeader}>
+              <View style={styles.threadCardHeaderLeft}>
+                <Text style={styles.threadCardTitle}>
+                  Summer
+                </Text>
+                <View style={styles.threadCardHeaderLeftDetail}>
+                  <Text style={styles.detailUpdateTime}>2 hours ago</Text>
+                  <View style={styles.detail}>
+                    <Image style={styles.detailImage} source={require('./statics/icon-user.png')} />
+                    <Text style={styles.detailText}>12</Text>
+                  </View>
+                  <View style={styles.detail}>
+                    <Image style={styles.detailImage} source={require('./statics/icon-comment.png')} />
+                    <Text style={styles.detailText}>56</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.threadCardHeaderRight}>
+                <Image source={require('./statics/photo.png')} />
+              </View>
+            </View>
+          </View>
+          <Text>Holi</Text>
+        </View>
+      )}
       <BottomBar active='threads' />
     </View>
   )
--- src/views/ThreadsList/statics/styles.js
+++ src/views/ThreadsList/statics/styles.js
@@ -7,6 +7,10 @@ export default StyleSheet.create({
     flex: 1,
   },
   contentContainer: {
+    paddingTop: 25,
+    paddingHorizontal: 15
+  },
+  emptyStateContainer: {
     paddingTop: 75,
     alignItems: 'center'
   },
@@ -22,5 +26,44 @@ export default StyleSheet.create({
     textAlign: 'center',
     paddingHorizontal: 52,
     marginBottom: 24
+  },
+  threadCard: {},
+  threadCardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 11,
+  },
+  threadCardHeaderLeft: {},
+  threadCardHeaderRight: {},
+  threadCardHeaderLeftDetail: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  threadCardTitle: {
+    fontFamily: "BentonSans",
+    fontSize: 30,
+    marginBottom: 7
+  },
+  detailUpdateTime: {
+    fontFamily: "BentonSans",
+    fontSize: 12,
+    color: "#9b9b9b",
+    marginRight: 24
+  },
+  detail: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginRight: 9,
+  },
+  detailImage: {
+    height: 12,
+    width: 12,
+    marginRight: 2
+  },
+  detailText: {
+    fontFamily: "BentonSans",
+    fontSize: 12,
+    lineHeight: 11,
   }
 })
