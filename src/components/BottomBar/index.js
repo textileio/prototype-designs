521f75b8b25857cedbe46af56317ef57e5f8af15
keyserfaty <keserfaty@gmail.com>
2018-07-21 17:22:45 -0300

    add active status for icons

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -9,13 +9,28 @@ const BottomBar = props => {
   return (
     <View style={styles.bottomBar}>
       <View style={active === 'threads' && styles.bottomBarIconActive}>
-        <Image style={styles.bottomBarIcon} source={require('./statics/icon-threads.png')} />
+        { active === 'threads' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-threads-active.png')} />
+        )}
+        { active !== 'threads' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-threads.png')} />
+        )}
       </View>
       <View style={active === 'wallet' && styles.bottomBarIconActive}>
-        <Image style={styles.bottomBarIcon} source={require('./statics/icon-wallet.png')} />
+        { active === 'wallet' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-wallet.png')} />
+        )}
+        { active !== 'wallet' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-wallet.png')} />
+        )}
       </View>
       <View style={active === 'feed' && styles.bottomBarIconActive}>
-        <Image style={styles.bottomBarIcon} source={require('./statics/icon-feed.png')} />
+        { active === 'feed' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-feed.png')} />
+        )}
+        { active !== 'feed' && (
+          <Image style={styles.bottomBarIcon} source={require('./statics/icon-feed.png')} />
+        )}
       </View>
     </View>
   )
