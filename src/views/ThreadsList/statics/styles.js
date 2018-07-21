ad81343fbd9bc852b42d3dc9d22d7f104065a3ad
keyserfaty <keserfaty@gmail.com>
2018-07-21 19:57:55 -0300

    create component for thread car

--- src/views/ThreadsList/statics/styles.js
+++ src/views/ThreadsList/statics/styles.js
@@ -26,49 +26,5 @@ export default StyleSheet.create({
     textAlign: 'center',
     paddingHorizontal: 52,
     marginBottom: 24
-  },
-  threadCard: {
-    marginBottom: 37
-  },
-  threadCardHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    marginBottom: 11,
-  },
-  threadCardHeaderLeft: {},
-  threadCardHeaderRight: {},
-  threadCardHeaderLeftDetail: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  threadCardTitle: {
-    fontFamily: "BentonSans",
-    fontSize: 30,
-    marginBottom: 7
-  },
-  detailUpdateTime: {
-    fontFamily: "BentonSans",
-    fontSize: 12,
-    color: "#9b9b9b",
-    marginRight: 24
-  },
-  detail: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    marginRight: 9,
-  },
-  detailImage: {
-    height: 12,
-    width: 12,
-    marginRight: 2
-  },
-  detailText: {
-    fontFamily: "BentonSans",
-    fontSize: 12,
-    lineHeight: 11,
-  },
-  threadCardBody: {
-
   }
 })
