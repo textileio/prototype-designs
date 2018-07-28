6686ecfa7547613600091e1b7865de9ab7eb0883
keyserfaty <keserfaty@gmail.com>
2018-07-28 11:57:10 -0300

    add styles to async permissions view

--- src/views/SyncPermissions/index.js
+++ src/views/SyncPermissions/index.js
@@ -1,5 +1,5 @@
 import React  from 'react'
-import { View, Text, Switch } from 'react-native'
+import { View, Text, Switch, ScrollView } from 'react-native'
 import ImageSc from 'react-native-scalable-image'
 
 import Button from '../../components/Button'
@@ -15,20 +15,42 @@ const SyncPermissions = props => {
         <ImageSc width={147} source={require('./statics/main-image.png')} />
         <Text style={styles.title}>To make everything run we need a few permissions</Text>
       </View>
-      <View style={styles.contentContainer}>
-
-        <View style={styles.listItem}>
-          <View>
-            <Text style={styles.itemTitle}>Camera Roll</Text>
-            <View style={styles.itemTexts}>
-              <Text style={styles.itemDescription}>Updates your photos</Text>
-              <ImageSc width={15} source={require('./statics/icon-info.png')} />
+      <ScrollView style={styles.contentContainer}>
+        <View style={styles.listContainer}>
+          <View style={styles.listItem}>
+            <View>
+              <Text style={styles.itemTitle}>Camera Roll</Text>
+              <View style={styles.itemTexts}>
+                <Text style={styles.itemDescription}>Updates your photos</Text>
+                <ImageSc width={15} source={require('./statics/icon-info.png')} />
+              </View>
             </View>
+            <Switch />
           </View>
-          <Switch />
+          <View style={styles.listItem}>
+            <View>
+              <Text style={styles.itemTitle}>Location</Text>
+              <View style={styles.itemTexts}>
+                <Text style={styles.itemDescription}>Updates your location</Text>
+                <ImageSc width={15} source={require('./statics/icon-info.png')} />
+              </View>
+            </View>
+            <Switch />
+          </View>
+          <View style={styles.listItem}>
+            <View>
+              <Text style={styles.itemTitle}>Background Location</Text>
+              <View style={styles.itemTexts}>
+                <Text style={styles.itemDescription}>Trigger app updates</Text>
+                <ImageSc width={15} source={require('./statics/icon-info.png')} />
+              </View>
+            </View>
+            <Switch />
+          </View>
         </View>
-
-        <Button text='Continue' onPress={() => {}} />
+      </ScrollView>
+      <View style={styles.footer}>
+        <Button style={styles.button} text='Continue' onPress={() => {}} />
       </View>
     </View>
   )
--- src/views/SyncPermissions/statics/styles.js
+++ src/views/SyncPermissions/statics/styles.js
@@ -11,14 +11,49 @@ export default StyleSheet.create({
     paddingTop: 28,
     paddingBottom: 51
   },
-  contentContainer: {},
+  title: {
+    fontFamily: "BentonSans",
+    fontSize: 20,
+    lineHeight: 30,
+    textAlign: 'center',
+    paddingHorizontal: 60
+  },
+  contentContainer: {
+    paddingHorizontal: 18
+  },
+  listContainer: {
+    borderBottomWidth: 1,
+    borderColor: '#d8d8d8'
+  },
   listItem: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+    borderTopWidth: 1,
+    borderColor: '#d8d8d8'
   },
-  itemTitle: {},
+  itemTitle: {
+    ...BentonSansBold(),
+    fontFamily: "BentonSans",
+    fontSize: 16,
+    lineHeight: 27,
+  },
+  itemDescription: {
+    fontFamily: "BentonSans",
+    fontSize: 16,
+    lineHeight: 27,
+    color: "#4a4a4a",
+    marginRight: 6
+  },
   itemTexts: {
     flexDirection: 'row',
+    alignItems: 'center',
   },
-  itemDescription: {}
+  footer: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 30,
+    paddingHorizontal: 28
+  }
 })
